2c6491b5037c580f387b0fe65d21f9be2156a6bd
MrrMD <52452257+MrrMD@users.noreply.github.com>
2024-03-24 15:45:39 +0300

    Initial commit

---
+++ src/main/java/ru/smallstash/idndemo/IdndemoApplication.java
@@ -0,0 +1,13 @@
+package ru.smallstash.idndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdndemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdndemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/smallstash/idndemo/IdndemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smallstash.idndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdndemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
